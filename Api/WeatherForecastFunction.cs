9c132e17ca5d7b2691b7cfe56365a8fa62597577
martyjacobsdev <martyjacobs2009@hotmail.com>
2022-09-29 00:54:10 +1000

    ...

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+
 using BlazorApp.Shared;
-using Azure.Data.Tables;
 
 namespace BlazorApp.Api
 {
@@ -36,18 +37,17 @@ namespace BlazorApp.Api
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
-            
-            // Construct a new TableClient using a TableSharedKeyCredential.
-            var client = new TableClient(
-                new Uri("https://mecc.table.core.windows.net/MaterEmergencyCareCentre"),
-                "MaterEmergencyCareCentre",
-                new TableSharedKeyCredential("mecc", connectionString));
+            var randomNumber = new Random();
+            var temp = 0;
 
-            var result = client.GetEntity<MaterEmergencyCareCentre>("mecc", "0001");
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = temp = randomNumber.Next(-20, 55),
+                Summary = GetSummary(temp)
+            }).ToArray();
 
             return new OkObjectResult(result);
-
         }
     }
 }
