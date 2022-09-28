47b1023714a16171ad6705770aef795271b0132c
martyjacobsdev <martyjacobs2009@hotmail.com>
2022-09-29 00:39:12 +1000

    ...

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-
 using BlazorApp.Shared;
 using Azure.Data.Tables;
-using System.Diagnostics;
-using Microsoft.WindowsAzure.Storage;
 
 namespace BlazorApp.Api
 {
